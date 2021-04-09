71cc3ee9e850f8f452d25af1c3b0b9db57d1494d
Bc. Viktor Šeňo <viktor.seno@student.tuke.sk>
2021-04-09 10:39:53 +0200

    1. Delete not useful component

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-    $(".configuration").click(function () {
+        var list = $('ul.configurations');
+        jQuery.getJSON("http://localhost:8080/getConfigs", function(data, status){
+        $.each(data, function (i){
+            $('<li/>').addClass('configuration').text(data[i]).appendTo(list);
+        });
+    });
+
+    $(".configurations").on("click", "li", function() {
         $('.configuration').removeClass('selected-configuration');
         $('.configuration').removeClass('success-configuration');
         $('.configuration').removeClass('wrong-configuration');
@@ -7,7 +14,7 @@ $(document).ready(function () {
     });
 
     $(".play-btn").click(function (){
-        if ($(".selected-configuration").length > 0){
+        if ($('.selected-configuration').length > 0){
             var name = {name: $(".selected-configuration").text()}
             jQuery.post("http://localhost:8080/play", name, function(data, status){
                 if("true".includes(data)){
@@ -23,4 +30,5 @@ $(document).ready(function () {
             alert("You choose any configuration !")
         }
     });
+
 });
